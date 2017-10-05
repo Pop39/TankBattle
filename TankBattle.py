c5591a6c388ab8be2853b29243fd606894e5d447
Panot Leelangamwongsa <panot.le@ku.th>
2017-10-05 22:46:49 +0700

    a

--- TankBattle.py
+++ TankBattle.py
@@ -1,16 +1,33 @@
 import arcade
+import arcade.key
  
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
  
-class TankWindow(arcade.Window):
+class TankBattleWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.BLACK)
+
+        self.tank_sprite = arcade.Sprite('images/tank.png')
+        self.tank_sprite.set_position(50,50)
+        self.bird_sprite = arcade.Sprite('images/bird.png')
+        self.bird_sprite.set_position(300,20)
+        self.wall_sprite = arcade.Sprite('images/stonewall.png')
+        self.wall_sprite.set_position(240,20)
+
+
+    def on_draw(self):
+        arcade.start_render()
+ 
+        self.tank_sprite.draw()
+        self.bird_sprite.draw()
+        self.wall_sprite.draw()
  
 def main():
-    window = TankWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window = TankBattleWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.set_window(window)
     arcade.run()
  
---
+++ models.py
@@ -0,0 +1,21 @@
+class Tank:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+ 
+    def update(self, delta):
+        if self.x > self.world.width:
+            self.x = 0
+        self.x += 5
+
+class World:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+ 
+        self.tank = Tank(self, width // 2, height // 2)
+ 
+ 
+    def update(self, delta):
+        self.tank.update(delta)
